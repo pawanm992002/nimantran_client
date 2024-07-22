3245b33e2b62f06c6bc4473912f339f908db9cdb
pawanm992002 <pawanm992002@gmail.com>
2024-07-23 02:46:59 +0530

    done

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -6,7 +6,7 @@ const LandingPage = () => {
     <div>
        <header className="bg-gradient-to-r from-pink-500 to-yellow-500 text-white text-center h-[100vh] py-8">
       <div className="container mx-auto w-full">
-        <h1 className="text-9xl font-bold">Nimantran Digital Designs</h1>
+        <h1 className="text-9xl font-bold">Nimantran Digital Designs demo</h1>
         <p className="mt-4">We are a one-stop solution for all your design needs.</p>
       </div>
     </header>
