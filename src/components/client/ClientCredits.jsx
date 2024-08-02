1d4fe65187b1ab33f115fb8f7074af1a4eaa1a36
Ayusht777 <talesarayush@gmail.com>
2024-08-02 11:45:37 +0530

    fix ui

--- src/components/client/ClientCredits.jsx
+++ src/components/client/ClientCredits.jsx
@@ -1,6 +1,24 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faWallet
+} from "@fortawesome/free-solid-svg-icons";
+const getStatusBadge = (status) => {
+  const statusStyles = {
+    completed: "text-green-500",
+    failed: "text-red-500 ",
+    pending: "text-yellow-500",
+  };
 
+  return (
+    <span
+      className={`${statusStyles[status] || "bg-gray-500 text-white"} rounded-full px-4 py-1 capitalize`}
+    >
+      {status}
+    </span>
+  );
+};
 const Transactions = () => {
   const token = localStorage.getItem("token");
   const [transactions, setTransactions] = useState([]);
@@ -53,21 +71,19 @@ const Transactions = () => {
       <div className="flex justify-between p-2 mb-6">
         <div className="flex flex-wrap space-x-4">
           <button
-            className={`px-6 py-2 text-lg font-semibold rounded-lg ${
-              tableSwitch === "transfer"
-                ? "bg-blue-500 text-white"
-                : "bg-gray-200 text-gray-800"
-            }`}
+            className={`px-6 py-2 text-lg font-semibold rounded-lg ${tableSwitch === "transfer"
+              ? "bg-blue-500 text-white"
+              : "bg-gray-200 text-gray-800"
+              }`}
             onClick={() => setTableSwitch("transfer")}
           >
             Transactions
           </button>
           <button
-            className={`px-6 py-2 text-lg font-semibold rounded-lg ${
-              tableSwitch === "spend"
-                ? "bg-blue-500 text-white"
-                : "bg-gray-200 text-gray-800"
-            }`}
+            className={`px-6 py-2 text-lg font-semibold rounded-lg ${tableSwitch === "spend"
+              ? "bg-blue-500 text-white"
+              : "bg-gray-200 text-gray-800"
+              }`}
             onClick={() => setTableSwitch("spend")}
           >
             Credit Spend in Editing
@@ -88,7 +104,10 @@ const Transactions = () => {
       ) : (
         <div>
           {filteredTransactions.length === 0 ? (
-            <div>No Transactions yet</div>
+            <div className="w-full flex justify-center items-center flex-col min-h-[55vh] h-full">
+              <div className=" size-48 flex justify-center items-center rounded-full bg-gray-200 text-8xl text-blue-500"><FontAwesomeIcon icon={faWallet} /></div>
+              <h1 className="mt-2 text-xl font-bold">No Transaction Yet</h1>
+              <p className="mt-0.5 text-gray-700">Search for another transition</p></div>
           ) : (
             <div className="overflow-x-auto h-[55vh]">
               <table className="min-w-full bg-white">
@@ -114,8 +133,7 @@ const Transactions = () => {
                           transaction.transactionDate
                         ).toLocaleDateString()}
                       </td>
-                      <td className="p-4">{transaction.status}</td>
-                    </tr>
+                      <td className="p-4 cursor-pointer">{getStatusBadge(transaction.status)}</td></tr>
                   ))}
                 </tbody>
               </table>
