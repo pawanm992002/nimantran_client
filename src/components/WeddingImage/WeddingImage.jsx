f40e94c71376e857c5eb37b67f166f2266bdf872
pawanm992002 <pawanm992002@gmail.com>
2024-08-23 10:45:47 +0530

    done

--- src/components/WeddingImage/WeddingImage.jsx
+++ src/components/WeddingImage/WeddingImage.jsx
@@ -97,21 +97,18 @@ export default function WeddingImage() {
 
   const handleVideoUpload = async (event) => {
     const inputFile = event.target.files[0];
-    const formData = new FormData();
-    formData.append("pawan", inputFile);
-
-
+    // const formData = new FormData();
+    // formData.append("pawan", inputFile);
 
     if (inputFile) {
       const videoPlayer = document.getElementById("videoPlayer");
-        console.log(inputFile)
-      const response = await axios.post(
-        `${process.env.REACT_APP_BACKEND_URL}/texts/image?eventId=${eventId}`,
-        formData,
-        {
-          headers: { Authorization: `Bearer ${token}` },
-        }
-      );
+      // const response = await axios.post(
+      //   `${process.env.REACT_APP_BACKEND_URL}/texts/image?eventId=${eventId}`,
+      //   formData,
+      //   {
+      //     headers: { Authorization: `Bearer ${token}` },
+      //   }
+      // );
       // console.log(response)
       const img = new Image();
       img.onload = () => {
@@ -215,48 +212,48 @@ export default function WeddingImage() {
     }
   };
 
-  useEffect(() => {
-    if (texts.length !== 0) {
-      var debouncedFetch = debounce(async () => {
-        try {
-          const response = await axios.post(
-            `${process.env.REACT_APP_BACKEND_URL}/texts/save?eventId=${eventId}`,
-            { texts },
-            {
-              headers: { Authorization: `Bearer ${token}` },
-            }
-          );
-          console.log(response.data);
-        } catch (error) {
-          console.error("Error saving texts:", error);
-        }
-      }, 10000);
-      debouncedFetch();
-      return () => {
-        debouncedFetch.cancel();
-      };
-    }
-  }, [texts]);
+  // useEffect(() => {
+  //   if (texts.length !== 0) {
+  //     var debouncedFetch = debounce(async () => {
+  //       try {
+  //         const response = await axios.post(
+  //           `${process.env.REACT_APP_BACKEND_URL}/texts/save?eventId=${eventId}`,
+  //           { texts },
+  //           {
+  //             headers: { Authorization: `Bearer ${token}` },
+  //           }
+  //         );
+  //         console.log(response.data);
+  //       } catch (error) {
+  //         console.error("Error saving texts:", error);
+  //       }
+  //     }, 10000);
+  //     debouncedFetch();
+  //     return () => {
+  //       debouncedFetch.cancel();
+  //     };
+  //   }
+  // }, [texts]);
 
-  useEffect(() => {
-    var getText = async () => {
-      try {
-        var response = await axios.get(
-          `${process.env.REACT_APP_BACKEND_URL}/texts/get?eventId=${eventId}`,
-          {
-            headers: { Authorization: `Bearer ${token}` },
-          }
-        );
-        // console.log(response.data[0].texts);
-        setTexts(response.data[0].texts);
-        console.log(texts);
-        return response.data[0].texts;
-      } catch (error) {
-        console.error("Error getting texts:", error);
-      }
-    };
-    getText();
-  }, []);
+  // useEffect(() => {
+  //   var getText = async () => {
+  //     try {
+  //       var response = await axios.get(
+  //         `${process.env.REACT_APP_BACKEND_URL}/texts/get?eventId=${eventId}`,
+  //         {
+  //           headers: { Authorization: `Bearer ${token}` },
+  //         }
+  //       );
+  //       // console.log(response.data[0].texts);
+  //       setTexts(response.data[0].texts);
+  //       console.log(texts);
+  //       return response.data[0].texts;
+  //     } catch (error) {
+  //       console.error("Error getting texts:", error);
+  //     }
+  //   };
+  //   getText();
+  // }, []);
 
   return (
     <div className="main">
